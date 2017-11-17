a2d05d2574444acb8a51f8aed5cd82bfa159b33c
jardevelopermode <33264960+jardevelopermode@users.noreply.github.com>
2017-11-17 15:58:06 -0600

    First version
    Simple .java file for jdk and jre 8 test

---
+++ hola.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package main;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author whiteHat
+ */
+public class hola{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        JOptionPane.showMessageDialog(null,"WEBOSS 2 PART");
+        
+    }
+    
+}
